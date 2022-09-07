193a015ad15890a98242ae643086d1d49fc25500
MAshrafM <ee.mashraf@gmail.com>
2022-09-07 03:48:56 +0200

    channel Detail Page function and style

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import { demoProfilePicture } from '../utils/constants';
 
 
-const ChannelCard = ({channelDetail}) => (
+const ChannelCard = ({channelDetail, marginTop}) => (
     <Box
       sx={{
         boxShadow: 'none',
@@ -14,7 +14,8 @@ const ChannelCard = ({channelDetail}) => (
         alignItems: 'center',
         width: { xs: '356px', md: '320px'},
         height: '326px',
-        margin: 'auto'
+        margin: 'auto',
+        marginTop: marginTop
       }}
     >
         <Link to={`/channel/${channelDetail?.id?.channelId}`}>
@@ -35,7 +36,8 @@ const ChannelCard = ({channelDetail}) => (
                     height: '180px',
                     width: '180px',
                     mb: 2,
-                    border: '1px solid #e3e3e3'
+                    border: '1px solid #e3e3e3',
+                    alignItems: 'center'
                   }}
                 />
                 <Typography variant="h6">
